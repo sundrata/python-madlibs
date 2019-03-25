15adf0307fee238b7c5e2bac0eadd13c2fb2f1ab
Carson Otto <carsonhockey15@gmail.com>
2019-03-25 17:18:24 -0500

    Little adlib that uses string formatting and recycles variables

--- madlib.py
+++ madlib.py
@@ -1,8 +1,10 @@
 #these raw inputs allow users to input their desired strings to the madlib
-inp1 = raw_input("A color:")
-inp2 = raw_input("A color:")
+name = raw_input("Name:")
+pronoun = raw_input("Pronoun:")
+noun = raw_input("Noun:")
 
 #madlib is the base madlib we will be inserting values to
-madlib = "Roses are %s, violets are %s" % (inp1, inp2)
+madlib = """%s went for a walk in the park. %s
+found a %s. %s decided to take the %s home.""" % (name, pronoun, noun, name, noun)
 
 print madlib
