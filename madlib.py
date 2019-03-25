1094b6fd5f24e0d9eff802dbdd990762de7d3a52
Carson Otto <carsonhockey15@gmail.com>
2019-03-25 17:14:43 -0500

    uses string formatting to fill in a basic adlib

--- madlib.py
+++ madlib.py
@@ -1,4 +1,8 @@
+#these raw inputs allow users to input their desired strings to the madlib
+inp1 = raw_input("A color:")
+inp2 = raw_input("A color:")
+
 #madlib is the base madlib we will be inserting values to
-madlib = "Roses are %s, violets are %s" % ('red', 'blue')
+madlib = "Roses are %s, violets are %s" % (inp1, inp2)
 
 print madlib
